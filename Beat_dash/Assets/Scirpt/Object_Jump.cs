1cf1b1440d41948c67aba8d094826ddd6e323407
erolf0123 <zaxscd123>
2020-01-11 03:48:31 +0900

    Object_create

---
+++ Beat_dash/Assets/Scirpt/Object_Jump.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Object_Jump : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow)) Jump();
+    }
+    void Jump()
+    {
+        transform.Translate(0.0f, 0.2f, 0.0f);
+    }
+}
