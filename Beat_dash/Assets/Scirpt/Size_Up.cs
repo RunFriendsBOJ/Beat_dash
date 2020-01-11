f78cc0d9d83b1a4fa51da90898a427601a6e3fa1
erolf0123 <zaxscd123>
2020-01-11 17:55:16 +0900

    Main

---
+++ Beat_dash/Assets/Scirpt/Size_Up.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Size_Up : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void PointerEnter()
+    {
+        transform.localScale = new Vector2(1.2f, 1.2f);
+    }
+
+    public void PointerExit()
+    {
+        transform.localScale = new Vector2(1f, 1f);
+    }
+    // Update is called once per frameå
+
+}
